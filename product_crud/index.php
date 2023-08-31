61ccd97245bdc7972f26eae2ee85b4bd1f4771f2
Omar Ochoa <omar@ochoawebdesign.com>
2023-08-30 19:59:41 -0700

    Crud - grab data from db and put into table

---
+++ product_crud/index.php
@@ -0,0 +1,58 @@
+<?php 
+
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'root', '');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$statement = $pdo->prepare('SELECT * FROM products ORDER BY create_date DESC');
+$statement->execute();
+$products = $statement->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="app.css">
+  </head>
+  <body>
+    <div class="container my-5">
+      <div class="col-lg-8 px-0">
+        <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">Image</th>
+                <th scope="col">Title</th>
+                <th scope="col">Price</th>
+                <th scope="col">Create Date</th>
+                <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($products as $i => $product) : ?>
+                <tr>
+                    
+                    <th><?php echo $i + 1; ?></th>
+                    <td><?php echo $product['image']; ?></td>
+                    <td><?php echo $product['title']; ?></td>
+                    <td><?php echo $product['price']; ?></td>
+                    <td><?php echo $product['create_date']; ?></td>
+                    <td>
+                        <button type="button" class="btn btn-sm btn-outline-primary">Edit</button>
+                        <button type="button" class="btn btn-sm btn-outline-danger">Delete</button>
+                    </td>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+            </table>
+      </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <script src="main.js"></script>
+  </body>
+</html>

3d20e74ffdbf4782a20319bbee3bda28e8c75a14
Omar Ochoa <omar@ochoawebdesign.com>
2023-09-05 18:55:01 -0700

    Header.php file

--- product_crud_better/index.php
+++ product_crud_better/index.php
@@ -15,15 +15,7 @@ $products = $statement->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <link rel="stylesheet" href="app.css">
-  </head>
+<?php include_once('views/partials/header.php'); ?>
   <body>
     <div class="container my-5">
       <div class="col-12 px-0">
